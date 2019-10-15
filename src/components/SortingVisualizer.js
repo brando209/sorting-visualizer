df99145405fdb7b98196b518c326bcd386d3ba4e
Branden McCarthy <bmccarthy2@ucmerced.edu>
2019-10-15 10:45:08 -0700

    Initial commit.

---
+++ src/components/SortingVisualizer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SortingVisualizer = () => {
+    return (
+        <div>
+            Sorting Visualizer
+        </div>
+    );
+};
+
+export default SortingVisualizer
